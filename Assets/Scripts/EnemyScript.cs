50fc4b1bdc8016edc7f99e47371757b89bef2037
Azure Biander <azure.biander@learnet.se>
2025-05-15 08:49:22 +0200

    Prefabs added

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
+    [SerializeField] BoxCollider visionBox;
+    [SerializeField] NavMeshAgent pathFinding;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        visionBox = transform.GetChild(0).GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+
     }
 }

3ed3a8cca4ab1a35a2678396ced0fbb5c0b95331
Otto hammarberg <Otto.hammarberg@elev.lbs.ga.se>
2025-06-08 14:05:45 +0200

    Test

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    [SerializeField] GameSettings gameSettings;
     public float mouseSensitivity = 100f;
     public Transform playerBody;
 
@@ -15,8 +16,8 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * gameSettings.mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * gameSettings.mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
